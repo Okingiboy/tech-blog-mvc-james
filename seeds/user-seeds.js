e6423624af06d86615d3ef6c56a3e941f8878b7d
“jameschukwu“ <okoriechukwu@yahoo.com>
2022-02-21 11:01:47 -0600

    modified files

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -2,45 +2,45 @@ const { User } = require('../models');
 
 const userData = [
     {
-        username: "martin_bour",
-        twitter: "martinb",
-        github: "martinb",
-        email: "martin_b@gmail.com",
+        username: "goeska_dodu",
+        twitter: "goeska",
+        github: "goeska",
+        email: "goeska_c@gmail.com",
         password: "p@ssword1"
     },
     {
-        username: "matt_b",
-        twitter: "mathewb",
-        github: "mathewb",
-        email: "mathew_b@gmail.com",
+        username: "lucas_m",
+        twitter: "lucasm",
+        github: "lucas",
+        email: "lucas_m@gmail.com",
         password: "p@ssword2"
     },
     {
-        username: "shaun_c",
-        twitter: "shaun",
-        github: "shaun",
-        email: "shaun_c@gmail.com",
+        username: "babuet_k",
+        twitter: "babuet",
+        github: "babuet",
+        email: "babuet_k@gmail.com",
         password: "p@ssword3"
     },
     {
-        username: "lee_n",
-        twitter: "leenorris",
-        github: "leenorris",
-        email: "lee_n@gmail.com",
+        username: "shaku_o",
+        twitter: "shaku",
+        github: "shakuo",
+        email: "shak_o@gmail.com",
         password: "p@ssword4"
     },
     {
-        username: "priya_r",
-        twitter: "priyaravi23",
-        github: "priyaravi23",
-        email: "priya_r@gmail.com",
+        username: "mariam_b",
+        twitter: "mariam64",
+        github: "mariam64",
+        email: "mariam_b@gmail.com",
         password: "p@ssword5"
     },
     {
-        username: "pooja",
-        twitter: "pooja_w",
-        github: "pooja",
-        email: "pooja@gmail.com",
+        username: "dockla",
+        twitter: "dockla_l",
+        github: "docklal",
+        email: "dockla_l@gmail.com",
         password: "p@ssword6"
     }
 ]
